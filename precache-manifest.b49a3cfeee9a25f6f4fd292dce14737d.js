62bd0f029aed5ae92660795ae184259823df04fa
Yesmith Tamayo Bustamante <yesmith.tamayo@CO-IT005839.local>
2020-02-26 17:07:54 -0500

    Updates

---
+++ precache-manifest.b49a3cfeee9a25f6f4fd292dce14737d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e48767e8de8be35c34a1dc17716b4b",
+    "url": "/yestamayocheapdomain.me/index.html"
+  },
+  {
+    "revision": "3b139d568a7bb9c42576",
+    "url": "/yestamayocheapdomain.me/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "eb3c44dcb779032a4062",
+    "url": "/yestamayocheapdomain.me/static/js/2.5ef9810e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/yestamayocheapdomain.me/static/js/2.5ef9810e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b139d568a7bb9c42576",
+    "url": "/yestamayocheapdomain.me/static/js/main.73ff0be5.chunk.js"
+  },
+  {
+    "revision": "b9a0f8435765f0b6b095",
+    "url": "/yestamayocheapdomain.me/static/js/runtime-main.100499c9.js"
+  }
+]);
